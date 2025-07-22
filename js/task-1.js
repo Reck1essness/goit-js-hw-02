74ca753334674198edd5935412018ddadf03b84c
Reck1essness <crossdmitry@gmail.com>
2025-07-22 17:49:33 +0300

    update

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const totalPrice = quantity * pricePerDroid;
     if (totalPrice > customerCredits) {
         return "insufficient funds!"; }
-        return "You ordered ${quantity} droids worth ${totalPrice} credits!";
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 
